96844a19216d478c5d931f59b1b70d63a9e19e60
root <root@ubuntu.cisco.com>
2014-04-09 14:43:33 -0700

    got debian distros working

--- lib/puppet/type/cobblerrepo.rb
+++ lib/puppet/type/cobblerrepo.rb
@@ -34,7 +34,7 @@ This rule would ensure that the kernel swappiness setting be set to '20'"
       when :i86pc
         :i386
       else
-        super
+        value
       end
     end
   end
--- manifests/add_distro.pp
+++ manifests/add_distro.pp
@@ -1,15 +1,24 @@
 # Define: cobbler::add_distro
-define cobbler::add_distro ($arch,$isolink) {
+define cobbler::add_distro ($arch,$isolink, $distro_os) {
   include cobbler
   $distro = $title
   $server_ip = $cobbler::server_ip
+
+  if $distro_os == 'rhel' {
+    $kernel = "${cobbler::distro_path}/${distro}/images/pxeboot/vmlinuz"
+    $initrd = "${cobbler::distro_path}/${distro}/images/pxeboot/initrd.img"
+  } elsif $distro_os == 'debian' {
+    $kernel = "${cobbler::distro_path}/${distro}/linux"
+    $initrd = "${cobbler::distro_path}/${distro}/initrd.gz"
+  }
+
   cobblerdistro { $distro :
     ensure  => present,
     arch    => $arch,
     isolink => $isolink,
     destdir => $cobbler::distro_path,
-    kernel  => "${cobbler::distro_path}/${distro}/images/pxeboot/vmlinuz",
-    initrd  => "${cobbler::distro_path}/${distro}/images/pxeboot/initrd.img",
+    kernel  => $kernel,
+    initrd  => $initrd,
     require => [ Service[$cobbler::service_name], Service[$cobbler::apache_service] ],
   }
   $defaultrootpw = $cobbler::defaultrootpw
